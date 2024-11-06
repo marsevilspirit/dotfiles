974cb0ac7617955e467b065bf18417bf0aebdda0
marsevilspirit <marsevilspirit@gmail.com>
2024-11-06 17:10:36 +0800

    update nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -638,7 +638,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          filetypes = { 'c', 'cpp' },
+        },
         gopls = {},
         pyright = {},
         rust_analyzer = {},
@@ -962,6 +964,7 @@ require('lazy').setup({
   require 'kickstart.plugins.go',
   require 'kickstart.plugins.bufferline',
   require 'kickstart.plugins.markdown-perview',
+  require 'kickstart.plugins.copilot',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
