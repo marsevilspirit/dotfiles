33bb373d92ddc11bc899dd7ca5dd1ea0331f030e
marsevilspirit <marsevilspirit@gmail.com>
2025-03-20 21:48:39 +0800

    add start.sh

---
+++ script/start.sh
@@ -0,0 +1,6 @@
+xrandr --output DP-2 --mode 2560x1600 --rate 240.00
+xrandr --output HDMI-0 --mode 2560x1440 --right-of DP-2 --rate 144.00
+xrandr --output DP-5 --mode 2560x1600 --right-of HDMI-0 --rotate left --rate 144.00
+feh --bg-center ~/图片/wallpaper/wallhaven-gpe92e_2560x1600.png \
+   ~/图片/wallpaper/wallhaven-d6y12l_2560x1600.png \
+   ~/图片/wallpaper/wallhaven-1pzzkg_1600x2560.png
