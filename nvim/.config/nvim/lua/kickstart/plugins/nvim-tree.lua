ba546a35e377b7f055bae8874621ab3e563d0e27
marsevilspirit <marsevilspirit@gmail.com>
2025-12-28 20:59:39 +0800

    chore: add OpenJDK path to fish user paths and remove Ghostty font family setting.

--- nvim/.config/nvim/lua/kickstart/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/nvim-tree.lua
@@ -10,13 +10,18 @@ return {
       -- 文件排序区别大小写
       sort_by = 'case_sensitive',
       view = {
+        -- 侧边栏的宽度
         width = 20,
       },
       renderer = {
+        -- 折叠空文件夹
         group_empty = true,
       },
       filters = {
-        dotfiles = true,
+        -- 显示隐藏文件
+        dotfiles = false,
+        -- 自定义过滤规则：只隐藏 .git 文件夹，不隐藏 .github
+        custom = { '^\\.git$' },
       },
     }
   end,
